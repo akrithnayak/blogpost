34ffb09fbf6f63f815cc5c682d40252b16c7c05c
Akrith H Nayak <akrithnayak2000@gmail.com>
2020-12-03 16:02:27 +0530

    Final

--- blogpost/settings.py
+++ blogpost/settings.py
@@ -140,5 +140,7 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get("EMAIL_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASS")
+print(EMAIL_HOST_USER)
+print(EMAIL_HOST_PASSWORD)
 
 django_heroku.settings(locals())
