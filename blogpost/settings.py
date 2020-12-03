509649ea6c9932b21228cd2b7c182bc8520bea74
Akrith H Nayak <akrithnayak2000@gmail.com>
2020-12-03 19:51:55 +0530

    Final

--- blogpost/settings.py
+++ blogpost/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
-ALLOWED_HOSTS = ['blog-post-nmam.herokuapp.com']
+ALLOWED_HOSTS = ['blogpostakrith.herokuapp.com']
 
 
 # Application definition
