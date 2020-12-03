3126fdbcbc50bc28a5a07cca41a95e9423be55c4
Akrith H Nayak <akrithnayak2000@gmail.com>
2020-12-03 15:19:02 +0530

    Environment variable changed

--- blogpost/settings.py
+++ blogpost/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'g0=8e1__si-=yz2_qy=9+&-57wq^e0w$kga=3m-2um(x+umge^'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['blog-post-nmam.herokuapp.com']
 
 
 # Application definition
